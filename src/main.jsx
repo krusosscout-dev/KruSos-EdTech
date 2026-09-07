f8daac071ebd729030f6d9e5377a96829aecb741
krusosscout-dev <krusosscout@gmail.com>
2026-09-07 14:28:12 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
+import kruSauceLogo from './assets/logo.js';
+
+// Enforce favicon to Kru Sauce Logo
+try {
+  let iconLink = document.querySelector("link[rel*='icon']");
+  if (!iconLink) {
+    iconLink = document.createElement('link');
+    iconLink.rel = 'icon';
+    document.head.appendChild(iconLink);
+  }
+  iconLink.type = 'image/jpeg';
+  iconLink.href = kruSauceLogo;
+
+  let appleLink = document.querySelector("link[rel='apple-touch-icon']");
+  if (!appleLink) {
+    appleLink = document.createElement('link');
+    appleLink.rel = 'apple-touch-icon';
+    document.head.appendChild(appleLink);
+  }
+  appleLink.href = kruSauceLogo;
+} catch (e) {}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
